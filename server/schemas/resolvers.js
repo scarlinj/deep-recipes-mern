166e5662ddd33d2e4c7ce6f13bedde16f70cb2b2
Jeongho Lee <jeongholee@Jeonghos-iMac-2.local>
2022-05-26 22:58:17 -0700

    add JWTs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,4 @@
+const { signToken } = require('../utils/auth');
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Recipe } = require("../models");
 
@@ -30,8 +31,9 @@ const resolvers = {
   Mutation: {
     addUser: async (parent, args) => {
       const user = await User.create(args);
+      const token = signToken(user);
     
-      return user;
+      return { token, user };
     },
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
@@ -46,8 +48,9 @@ const resolvers = {
         throw new AuthenticationError('Incorrect credentials');
       }
     
-      return user;
-    },
+      const token = signToken(user);
+      return { token, user };
+    }
     },
   };
 // const resolvers = {
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -35,8 +35,12 @@ const typeDefs = gql`
     recipe(_id: ID!): Recipe
   }
   type Mutation {
-    login(email: String!, password: String!): User
-    addUser(username: String!, email: String!, password: String!): User
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+  }
+  type Auth {
+    token: ID!
+    user: User
   }
 `;
 // export the typeDefs
